8a1a773de616ce2bd9c6476d3d03b8f73184ed55
Star Mercy <abhimittal19@gmail.com>
2017-05-03 15:33:28 +0530

    first commit

---
+++ app/src/main/java/bob/com/example/root/blazeguide/utility/Config.java
@@ -0,0 +1,10 @@
+package bob.com.example.root.blazeguide.utility;
+
+/**
+ * Created by root on 27/4/17.
+ */
+
+public class Config {
+    public static final String YOUTUBE_API_KEY = "AIzaSyD-eHXFsOfw4Uuw07NMO0OlaTRaCzdLSts";
+
+}
